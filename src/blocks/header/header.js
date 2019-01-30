7308fea83503853779fbe01239c711d8b9bb5a7b
Zheka-ded <11vitamin11@rambler.ru>
2019-01-30 22:47:20 +0200

    30/01/19(что-то опять пошло не так)

---
+++ src/blocks/header/header.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() {
+
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', '/src/plugins/particles.js', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+});
--- src/plugins/particles.js
+++ src/plugins/particles.js
@@ -118,15 +118,3 @@ particlesJS('particles-js',
   }
 
 );
-
-
-
-jQuery(document).ready(function() {
-
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles-js', '/src/plugins/particles.js', function() {
-    console.log('callback - particles.js config loaded');
-  });
-
-});
-
