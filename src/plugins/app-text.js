81534780bb535c966064788f38cf5d0167c94e4e
Zheka-ded <11vitamin11@rambler.ru>
2019-02-03 16:06:03 +0200

    03/03/19(маленькие корректировки первой страницы)

--- src/plugins/app-text.js
+++ src/plugins/app-text.js
@@ -22,13 +22,15 @@ function Gravity(id){
 
   this.useBound = this.checkBound();
   this.colors = [
+      '#ffffff','#000000','#686868',
+      '#3c546c','#3f51b5','#d23e30',
       '#f44336','#e91e63','#9c27b0',
       '#673ab7','#3f51b5','#2196f3',
       '#03a9f4','#00bcd4','#009688',
       '#4caf50','#8bc34a','#cddc39',
       '#ffeb3b','#ffc107','#ff9800',
       '#ff5722','#795548','#9e9e9e',
-      '#607d8b'
+      '#607d8b','#0de5c2'
   ];
 
   this.randomColor = function(){
